4617c2863d93d4d76644c8b054b9402b20049251
mikhail <kazanmichail@hotmail.com>
2025-02-17 18:43:05 +0100

    Add Transaction Controller

--- paypalserversdk/models/__init__.py
+++ paypalserversdk/models/__init__.py
@@ -114,6 +114,7 @@ __all__ = [
     'tax_info',
     'three_d_secure_authentication_response',
     'token',
+    'transactions_details',
     'order_tracker_response',
     'order_tracker_item',
     'trustly_payment_object',
